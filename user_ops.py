d1c10ee50e15d65c9f826592e7347e9ffd166bf8
Berat Reha Sonmez <beratson@gmail.com>
2018-05-12 20:46:22 +0300

    Method definitions, console cleanUp user_ops

--- user_ops.py
+++ user_ops.py
@@ -14,13 +14,22 @@ session = DBSession()
 
 
 def registerUser(login_session):
+    """
+    Constructs User object from login_session
+    Checks if the user exists, if so returns User
+    If not, creates a new user with login_session data
+    """
     ls = login_session
+    """
+    Console tests for DEBUG
     print("in registerUser")
     print(ls)
+    """
     email = ls['email']
     existingUser = session.query(User).filter_by(email=email).first()
     if existingUser is not None:
-        print("User already in DB => %s") % existingUser.name
+        # Console test
+        # print("User already in DB => %s") % existingUser.name
         return existingUser
     else:
         newUser = User(email=email)
@@ -32,9 +41,11 @@ def registerUser(login_session):
         elif login_session['access_token_github']:
             newUser.access_token_github = ls['access_token_github']
         else:
+            # console log
             print("No Access token from SignIn attempt found!")
 
         session.add(newUser)
         session.commit()
-        print("New User succesfully added to DB")
+        # console log
+        # print("New User succesfully added to DB")
         return newUser
